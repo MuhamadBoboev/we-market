49dbdc4b901ea83da0a23db3d568407fb44e0e72
Muhammad <92302413+MuhamadBoboev@users.noreply.github.com>
2024-06-04 19:52:29 +0500

    initial project - 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@widgets/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        </body>
     </html>
   );
 }
---
+++ src/widgets/header/ui/Header/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { HandySvg } from 'handy-svg'
+import classes from './header.module.scss'
+
+export const Header = () => {
+    return <header>
+        <div>
+            <HandySvg src='/assets/icons/logo.svg' />
+
+        </div>
+    </header>
+}
