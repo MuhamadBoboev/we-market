6d6b2a987c571be2a739df439b4afb7d7ee2208d
Muhammad <92302413+MuhamadBoboev@users.noreply.github.com>
2024-06-18 20:29:55 +0500

    change card

--- src/entities/product/ui/Card/index.tsx
+++ src/entities/product/ui/Card/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import classes from './card.module.scss'
-
+interface Props {
+    
+}
 export const Card = () => {
     return <article>
         <div className={classes.bl_img} >
@@ -10,10 +12,11 @@ export const Card = () => {
                 height={220}
                 alt='product'
                 />
+            
         </div>
         <div className={classes.content} >
             <h3 className={classes.title} >
-                
+
             </h3>
             <p className={classes.text} >
 
