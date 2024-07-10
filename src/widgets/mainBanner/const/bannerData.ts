11f6dd658326ee1b79e307fc23925e8fea3cc51c
Muhammad <92302413+MuhamadBoboev@users.noreply.github.com>
2024-07-10 20:26:17 +0500

    change main banner

---
+++ src/widgets/mainBanner/const/bannerData.ts
@@ -0,0 +1,39 @@
+import { IBanner } from "../model/banner";
+
+export const bannerData: IBanner[] = [
+    {
+        id: 1,
+        image: '/assets/images/banners/banner-1.png',
+        link: null,
+        order: 1,
+        type: 'main'
+    },
+    {
+        id: 2,
+        image: '/assets/images/banners/banner-1.png',
+        link: null,
+        order: 2,
+        type: 'main'
+    },
+    {
+        id: 3,
+        image: '/assets/images/banners/banner-1.png',
+        link: null,
+        order: 3,
+        type: 'main'
+    },
+    {
+        id: 4,
+        image: '/assets/images/banners/banner-1.png',
+        link: null,
+        order: 2,
+        type: 'main'
+    },
+    {
+        id: 5,
+        image: '/assets/images/banners/banner-1.png',
+        link: null,
+        order: 3,
+        type: 'main'
+    },
+]
